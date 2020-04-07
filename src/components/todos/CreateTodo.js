0dff9491857fd620057a856b2cf6c1e083b1c4a5
scrabill <shannon.crabill@gmail.com>
2020-04-07 16:45:50 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -10,15 +10,19 @@ class CreateTodo extends Component {
     };
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-  }
 
   handleChange(event) {
     this.setState({
       text: event.target.value
     });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+    this.setState({
+      text: '',
+    })
   }
 
   render() {
